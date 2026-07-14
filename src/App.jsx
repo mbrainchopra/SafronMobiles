ee8863c092a55c3b8cfa1f74857f09b1e6d2cf08
Michael <michael@alphasoftz.in>
2026-07-14 23:38:01 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import "./styles/color.css";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-         <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
